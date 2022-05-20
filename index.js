0d39b3d94bf38026a37d4fac2e33dd080ff38b1d
saifur550 <srstap550@gmail.com>
2022-05-20 11:30:07 +0600

    heroku server unable to access

--- index.js
+++ index.js
@@ -41,10 +41,28 @@ async function run(){
         const serviceCollection =client.db('doctors_portal').collection('services')
         const bookingCollection =client.db('doctors_portal').collection('bookings')
         const userCollection =client.db('doctors_portal').collection('users')
+        const doctorCollection =client.db('doctors_portal').collection('doctors')
+
+
+        const verifyAdmin = async (req, res, next )=>{
+          const requester = req.decoded.email;
+          const requesterAccount = await userCollection.findOne({email:requester})
+          if(requesterAccount.role === 'admin'){
+            next()
+          }
+
+          else{
+            return res.status(403).send({ message: 'Forbidden access' })
+          }
+
+        }
+
+
+
 
         app.get('/services', async (req, res)=>{
             const query = {};
-            const cursor = serviceCollection.find(query)
+            const cursor = serviceCollection.find(query).project({name:1})
             const services = await cursor.toArray()
             res.send(services)
 
@@ -59,30 +77,17 @@ async function run(){
           res.send(users)
         })
 
-        app.put('/user/admin/:email' , verifyJWT,   async(req, res)=>{
-        const email = req.params.email;
-        const requester = req.decoded.email;
-        const requesterAccount = await userCollection.findOne({email:requester})
-
-        if(requesterAccount.role === 'admin'){
-
+        app.put('/user/admin/:email' , verifyJWT,  verifyAdmin,  async(req, res)=>{
+          const email = req.params.email;
           const filter =  {email: email};
-          // create a document that sets the plot of the movie
           const updateDoc = {
            $set:{role:'admin'},
           };
           const result = await userCollection.updateOne(filter, updateDoc);
           res.send(result);
 
-        }
-
-        else{
-         res.status(403).send({ message: 'Forbidden access' })
-
-        }
-
-
         })
+
 
       app.get('/admin/:email', async( req, res)=>{
         const email =req.params.email;
@@ -144,14 +149,7 @@ async function run(){
 
 
 
-      /**
-     * API Naming Convention
-     * app.get('/booking') // get all bookings in this collection. or get more than one or by filter
-     * app.get('/booking/:id') // get a specific booking 
-     * app.post('/booking') // add a new booking
-     * app.patch('/booking/:id) //
-     * app.delete('/booking/:id) //
-    */
+   
 
 
       app.get('/booking', verifyJWT ,  async (req, res)=>{
@@ -187,6 +185,12 @@ async function run(){
          return res.send({success:true, result});
 
 
+      })
+
+      app.post('/doctor' , verifyJWT, verifyAdmin,  async ( req, res)=>{
+        const doctor = req.body
+        const result = await doctorCollection.insertOne(doctor)
+        res.send(result)
       })
         
         
